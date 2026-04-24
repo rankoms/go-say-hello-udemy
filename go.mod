405de3a35fca4818d472ac991c279ae44d42bf2b
rankoms <rankom202@gmail.com>
2026-04-24 16:28:16 +0700

    update say hello with param

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/rankoms/go-say-hello-udemy
+module github.com/rankoms/go-say-hello-udemy/v2
 
 go 1.25.0
--- sayhello.go
+++ sayhello.go
@@ -1,5 +1,5 @@
 package gosayhelloudemy
 
-func SayHello() string {
-	return "Hello, World!"
+func SayHello(name string) string {
+	return "Hello, " + name + "!"
 }
